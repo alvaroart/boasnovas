eedb1176d145bb46b81e58c7b7d61354f1d178a9
alvaroart <alvaro.rosa1985@gmail.com>
2022-05-15 13:42:57 -0300

    tmpl testes

--- membros/main.go
+++ membros/main.go
@@ -74,6 +74,188 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 }
 
+//Função show exibe apenas um resultado
+func Show(w http, ResponseWriter, r *http.Request) {
+	db := dbConn()
+
+	//pegar o ID do parametro da URL
+	nId := r.URL.Query().Get("id")
+
+	//usa o ID para fazer a consulta e tratar erros
+	selDB, err := db.Query("SELECT * FROM names WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//monta a struct para ser utulizada no template
+	n := Names{}
+
+	// executa a estrutura de repetição pegando todos os valores do banco
+	for selDB.Next() {
+		//armazena os valore nas variaveis
+		var id int
+		var name, email string
+
+		//faz o scan do select
+		err = selDB.Scan(&id, &name, &email)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		// Envia os resultados para a struct
+		n.Id = id
+		n.Name = name
+		n.Email = email
+	}
+
+	// Mostra o template
+	tmpl.ExecuteTemplate(w, "Show", n)
+
+	// Fecha a conexão
+	defer db.Close()
+
+}
+
+// Função New apenas exibe o formulário para inserir novos dados
+func New(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "New", nil)
+}
+
+// Função Edit, edita os dados
+func Edit(w http.ResponseWriter, r *http.Request) {
+	// Abre a conexão com banco de dados
+	db := dbConn()
+
+	// Pega o ID do parametro da URL
+	nId := r.URL.Query().Get("id")
+
+	selDB, err := db.Query("SELECT * FROM names WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Monta a struct para ser utilizada no template
+	n := Names{}
+
+	// Realiza a estrutura de repetição pegando todos os valores do banco
+	for selDB.Next() {
+		//Armazena os valores em variaveis
+		var id int
+		var name, email string
+
+		// Faz o Scan do SELECT
+		err = selDB.Scan(&id, &name, &email)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		// Envia os resultados para a struct
+		n.Id = id
+		n.Name = name
+		n.Email = email
+	}
+
+	// Mostra o template com formulário preenchido para edição
+	tmpl.ExecuteTemplate(w, "Edit", n)
+
+	// Fecha a conexão com o banco de dados
+	defer db.Close()
+}
+
+// Função Insert, insere valores no banco de dados
+func Insert(w http.ResponseWriter, r *http.Request) {
+
+	//Abre a conexão com banco de dados usando a função: dbConn()
+	db := dbConn()
+
+	// Verifica o METHOD do fomrulário passado
+	if r.Method == "POST" {
+
+		// Pega os campos do formulário
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+
+		// Prepara a SQL e verifica errors
+		insForm, err := db.Prepare("INSERT INTO names(name, email) VALUES(?,?)")
+		if err != nil {
+			panic(err.Error())
+		}
+
+		// Insere valores do formulario com a SQL tratada e verifica errors
+		insForm.Exec(name, email)
+
+		// Exibe um log com os valores digitados no formulário
+		log.Println("INSERT: Name: " + name + " | E-mail: " + email)
+	}
+
+	// Encerra a conexão do dbConn()
+	defer db.Close()
+
+	//Retorna a HOME
+	http.Redirect(w, r, "/", 301)
+}
+
+// Função Update, atualiza valores no banco de dados
+func Update(w http.ResponseWriter, r *http.Request) {
+
+	// Abre a conexão com o banco de dados usando a função: dbConn()
+	db := dbConn()
+
+	// Verifica o METHOD do formulário passado
+	if r.Method == "POST" {
+
+		// Pega os campos do formulário
+		name := r.FormValue("name")
+		email := r.FormValue("email")
+		id := r.FormValue("uid")
+
+		// Prepara a SQL e verifica errors
+		insForm, err := db.Prepare("UPDATE names SET name=?, email=? WHERE id=?")
+		if err != nil {
+			panic(err.Error())
+		}
+
+		// Insere valores do formulário com a SQL tratada e verifica erros
+		insForm.Exec(name, email, id)
+
+		// Exibe um log com os valores digitados no formulario
+		log.Println("UPDATE: Name: " + name + " |E-mail: " + email)
+	}
+
+	// Encerra a conexão do dbConn()
+	defer db.Close()
+
+	// Retorna a HOME
+	http.Redirect(w, r, "/", 301)
+}
+
+// Função Delete, deleta valores no banco de dados
+func Delete(w http.ResponseWriter, r *http.Request) {
+
+	// Abre conexão com banco de dados usando a função: dbConn()
+	db := dbConn()
+
+	nId := r.URL.Query().Get("id")
+
+	// Prepara a SQL e verifica errors
+	delForm, err := db.Prepare("DELETE FROM names WHERE id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Insere valores do form com a SQL tratada e verifica errors
+	delForm.Exec(nId)
+
+	// Exibe um log com os valores digitados no form
+	log.Println("DELETE")
+
+	// Encerra a conexão do dbConn()
+	defer db.Close()
+
+	// Retorna a HOME
+	http.Redirect(w, r, "/", 301)
+}
+
 func main() {
 
 	var tmpl = template.Must(template.ParseGlob("tmpl/*"))
